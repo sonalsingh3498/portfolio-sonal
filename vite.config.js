f8779cb5c02aca284cf73397932bdf5f7b2fbfdd
Sonal Singh <sonalsingh@Sonals-MacBook-Pro.local>
2025-06-30 20:04:30 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-    base: '/portfolio-sonal/',
-    plugins: [react()],
-  })
-  
+  base: '/portfolio-sonal/', // 👈 Required for GitHub Pages
+  plugins: [react()],
+});
